24fa3870094bc778d35c09e13f968c235e151eff
AGoodMan <643012918@qq.com>
2021-08-10 22:08:50 +0800

    :wrench: 修改默认的http转换器，暂时兼容服务端返回code码

--- lib/src/http/default_http_transformer.dart
+++ lib/src/http/default_http_transformer.dart
@@ -2,15 +2,23 @@ import 'package:dio/dio.dart';
 import 'package:flutter_app/src/http/http_response.dart';
 import 'package:flutter_app/src/http/http_transformer.dart';
 
+/// 默认的HTTP转换器
 class DefaultHttpTransformer extends HttpTransformer {
+  /// todo 这里先注释掉
+  /// 服务端异常与dart封装异常不匹配
+  /// 后续以服务端异常为准
+  /// 移动端只保留一些基础异常
   @override
   HttpResponse parse(Response response) {
-
-    if (response.data["code"] == 1 && response.data["msg"] == "成功") {
+    // if (response.data["code"] == 1 && response.data["msg"] == "成功") {
+    if (response.data["code"] == "00000" && response.data["msg"] == "OK") {
       return HttpResponse.success(response.data["data"]);
     } else {
       return HttpResponse.failure(
-          errorMsg: response.data["message"], errorCode: response.data["code"]);
+        // errorMsg: response.data["message"], errorCode: response.data["code"]);
+        errorMsg: response.data["message"],
+        errorCode: -1,
+      );
     }
   }
 
